64d2b9f0264c4ee09c6cf6aa13b6ae153d370ffe
RAGAVE.I <95114993+Ragave103@users.noreply.github.com>
2023-09-12 09:49:26 +0530

    Update App.jsx

--- client/speech2code/src/components/App.jsx
+++ client/speech2code/src/components/App.jsx
@@ -3,11 +3,23 @@ import CodeEditor from "./CodeEditor.jsx";
 import Editorr from "./Editorr.jsx";
 import Web from "./Web.jsx";
 import Sidebar from "./Sidebar.jsx";
-
+import Sample from "./Sample.jsx";
+import Loginpage from "./Loginpage.jsx";
+import SignUp from "./auth/SignUp";
+import AuthDetails from "./AuthDetails";
+import SignUpPage from "./SignUpPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Web />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Loginpage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
+
+          <Route path="/dashboard" element={<Web />} />
+        </Routes>
+      </Router>
     </>
   );
 }
